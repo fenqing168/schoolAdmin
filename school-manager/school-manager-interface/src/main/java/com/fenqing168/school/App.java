8057c4b4e4b262b82667af2fac2935a814ce22f6
fenqing <1286976336@qq.com>
2019-01-02 13:25:39 +0800

    初次导入

---
+++ school-manager/school-manager-interface/src/main/java/com/fenqing168/school/App.java
@@ -0,0 +1,13 @@
+package com.fenqing168.school;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
